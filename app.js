87ac9e3769a6522aa4ffc193c069a65938af4308
Maxwell Ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2021-09-29 18:11:55 +0100

    served views as static

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use("/public", express.static(__dirname + "/public"));
+app.use("/views", express.static(__dirname + "/views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,8 +1,6 @@
-const path = require("path");
-
 const indexControl = (req, res) => {
   // res.render("index");
-  res.sendFile(path.resolve("../../views/index.html"));
+  res.sendFile("./index.html");
 };
 
 module.exports = indexControl;
